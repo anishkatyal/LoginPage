4b4142c2fdd34e2d59415a4ac7118f6407005692
anishkatyal1 <anishkatyal.1p1@gmail.com>
2020-03-11 17:57:38 +0530

    updated project after completion of task 2

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -7,7 +7,8 @@ class Dashboard extends Component{
     render(){
         return(
          <div>   <h1 className="main">Welcome to shopping page</h1>
-           <Link to="/login"> <button className="but">Logout</button></Link>
+          <button className="cart">Cart</button> 
+          <Link to="/login"> <button className="logout">Logout</button></Link>
             </div>
         )
     }
